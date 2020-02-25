767ebc012cdd9b492087d562e8adbd528506eae2
Luis Polasek <lpolasek@gmail.com>
2020-02-25 12:30:56 -0300

    Changed aliases serializer from a string, to a token generated list.

--- movies/models.py
+++ movies/models.py
@@ -5,8 +5,12 @@ class Person(models.Model):
     last_name = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
 
-    # TODO: convert to string list
-    aliases = models.CharField(max_length=500, blank=True)
+    aliases = models.CharField(
+        max_length=500,
+        blank=True,
+        help_text='This field is converted to a list using ' +
+            '| as a separator'
+    )
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
--- movies/serializers.py
+++ movies/serializers.py
@@ -1,9 +1,72 @@
+from django.db import models
 from rest_framework import serializers
 
 from movies.models import Movie, Person
 
 
+class TokenizedListField(serializers.ListField):
+    """
+    Saves a list of serializers as a token separated string.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.token = kwargs.pop('token', '|')
+        super().__init__(*args, **kwargs)
+
+    def to_internal_value(self, data):
+        """
+        List of dicts of native values <- List of dicts of primitive datatypes.
+        """
+        if html.is_html_input(data):
+            data = html.parse_html_list(data, default=[])
+
+        if not isinstance(data, list):
+            message = self.error_messages['not_a_list'].format(
+                input_type=type(data).__name__
+            )
+            raise ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='not_a_list')
+
+        if not self.allow_empty and len(data) == 0:
+            message = self.error_messages['empty']
+            raise ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='empty')
+
+        ret = []
+        errors = []
+
+        for item in data:
+            try:
+                validated = self.child.run_validation(item)
+            except ValidationError as exc:
+                errors.append(exc.detail)
+            else:
+                ret.append(validated)
+                errors.append({})
+
+        if any(errors):
+            raise ValidationError(errors)
+
+        return self.token.join(ret)
+
+    def to_representation(self, data):
+        """
+        List of object instances -> List of dicts of primitive datatypes.
+        """
+        # Dealing with nested relationships, data can be a Manager,
+        # so, first get a queryset from the Manager if needed
+        iterable = data.all() if isinstance(data, models.Manager) else data
+
+        return [
+            self.child.to_representation(item) for item in iterable.split(self.token)
+        ]
+
+
 class PersonSerializer(serializers.HyperlinkedModelSerializer):
+    aliases = TokenizedListField(child=serializers.CharField())
+
     class Meta:
         model = Person
         fields = [
